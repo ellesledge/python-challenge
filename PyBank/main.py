7e4959724f7c63dad2c4103c02bcc1f591e3cb75
ellesledge <69279540+ellesledge@users.noreply.github.com>
2020-09-15 13:55:18 -0400

    main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,29 +7,22 @@ print('---------------------------------------')
 
 with open(budget_data, 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
-    next(csv_file)
-    months = len(list(csv_file))
+    next(reader)
+    data = list(reader)
+    months = len(data)
     print(f'Total Months: {months}')
+ 
+net = []
+for row in data:
+    net.append(int(row[1]))
+print(f'Total: ${sum(net)}')
 
-with open(budget_data, 'r') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
-    next(csv_file)
-    net = []
-    for row in reader:
-        net.append(int(row[1]))
+Changes = 0
+for i in range(1,len(data)):
+    # print(int(data[i][1]))
+    dif = int(data[i][1]) - int(data[i-1][1])
+    Changes += dif
+Average_change = Changes/(len(data)-1)
+Average_change_format= "${:,.2f}".format(Average_change)
+print(f'Average Change: {Average_change_format}')
 
-    print(f'Total: ${sum(net)}')
-
-with open(budget_data, 'r') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
-    next(csv_file)
-    net = []
-    for row in reader:
-        net. append(int(row[1]))
-    Average = len(net) / sum(net)
-    # print(Average)
-    print(f'Average Change: ${Average(net)}')
-
-   
-    
-    
